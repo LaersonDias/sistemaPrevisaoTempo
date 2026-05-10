7b90c4479f85d5f8855bda6e1898cf9607c3207b
Laerson Dias <josemarchitungo5@gmail.com>
2026-05-10 22:17:24 +0100

    reoslve merge conflict e remove test file

---
+++ frontend/client/src/app/modules/dashboard/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  city: string = '';
+  weatherData: any = null;
+
+  constructor(private weatherService: WeatherService) {}
+
+  searchWeather() {
+    if (!this.city) return;
+
+    this.weatherService.getWeather(this.city)
+      .subscribe({
+        next: (data) => {
+          this.weatherData = data;
+        },
+        error: (err) => {
+          console.log('Erro ao buscar clima:', err);
+        }
+      });
+  }
+}
