66141216ee995fd64c6719c0da2f4cfbb31115a4
Laerson Dias <josemarchitungo5@gmail.com>
2026-05-10 23:19:18 +0100

    interface moderna de previsão do tempo

--- frontend/client/src/app/modules/dashboard/home/home.ts
+++ frontend/client/src/app/modules/dashboard/home/home.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { WeatherService } from '../../../services/weather.service';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -14,16 +24,21 @@ export class HomeComponent {
   constructor(private weatherService: WeatherService) {}
 
   searchWeather() {
+
     if (!this.city) return;
 
     this.weatherService.getWeather(this.city)
       .subscribe({
+
         next: (data) => {
           this.weatherData = data;
         },
+
         error: (err) => {
           console.log('Erro ao buscar clima:', err);
         }
+
       });
+
   }
 }
